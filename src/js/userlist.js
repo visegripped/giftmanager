72a1bb91a1dc59bacc6c474e02cfadbf5a5f36ae
visegripped <nogitspam@visegripped.com>
2016-08-22 07:57:32 -0700

    Get MVP wrapped up for users

--- src/js/userlist.js
+++ src/js/userlist.js
@@ -1,108 +1,110 @@
-   (function(React){
+(function(React){
 
-        "use strict";
+     "use strict";
 
-        const thisUserID = 58627; //set this after authentication.
+     const thisUserID = 58627; //set this after authentication.
+     const customEvents = {
+       userSelected : new CustomEvent("UserList.userSelected"),
+       menuOpened : new CustomEvent("UserList.menuOpened"),
+       menuClosed : new CustomEvent("UserList.menuClosed")
+     };
 
-        var UserMenu = React.createClass({
+     var UserMenu = React.createClass({
 
-            loadUsersFromServer: function() {
-                $.ajax({
-                    url: this.props.usersUrl,
-                    dataType: 'json',
-                    cache: false,
-                    success: function(data) {
-                        this.setState({users: data});
-                    }.bind(this),
-                    error: function(xhr, status, err) {
-                        console.error(this.props.usersUrl, status, err.toString());
-                    }.bind(this)
-                });
-            },
+         loadUsersFromServer: function() {
+             $.ajax({
+                 url: this.props.usersUrl,
+                 dataType: 'json',
+                 cache: false,
+                 success: function(data) {
+                     this.setState({users: data});
+                 }.bind(this),
+                 error: function(xhr, status, err) {
+                     console.error(this.props.usersUrl, status, err.toString());
+                 }.bind(this)
+             });
+         },
 
-            getInitialState: function() {
-                return {
-                    users : [],
-                    subjectUID : thisUserID,
-                    menuVisibility : "closed"  //valid values are open and closed
-                };
-            },
+         getInitialState: function() {
+             return {
+                 users : [],
+                 subjectUID : thisUserID,
+                 menuVisibility : "closed"  //valid values are open and closed
+             };
+         },
 
-            handleUserChange : function(user) {
-                this.setState({menuVisibility: 'closed'});
-                this.setState({subjectUID: user.userid});
-            },
+         handleUserChange : function(user) {
+             this.setState({menuVisibility: 'closed'});
+             this.setState({subjectUID: user.userid});
+             dispatchEvent(customEvents.userSelected);
+         },
 
-            toggleMenuVisibility: function() {
-                this.setState({menuVisibility: this.state.menuVisibility === 'open' ? 'closed' : 'open'});
-            },
+         toggleMenuVisibility: function() {
+             this.setState({menuVisibility: this.state.menuVisibility === 'open' ? 'closed' : 'open'});
+         },
 
-            componentDidMount: function() {
-                this.loadUsersFromServer();
-            },
+         componentDidMount: function() {
+             this.loadUsersFromServer();
+         },
 
-            render: function() {
-                return (
-                    <nav className={"navbar-"+this.state.menuVisibility}>
-                        <MenuToggleButton onButtonClick={this.toggleMenuVisibility}  />
-                        <UserList onUserChange={this.handleUserChange} data={this.state} />
-                    </nav>
-                );
-            }
-        });
-
-
-        var UserList = React.createClass({
-            render: function() {
-            //console.log("UserList: " , this);
-                let userEvent = this.props.onUserChange;
-                let subjectUID = this.props.data.subjectUID;
-                let userNodes = this.props.data.users.map(function(user) {
-                    return (
-                        <User user={user} key={user.userid} onClick={userEvent} subjectUID={subjectUID}>
-                        {user.userid}
-                        </User>
-                    );
-                });
-                return (
-                    <ul className="nav navbar-nav float-right" id="nav">
-                        {userNodes}
-                    </ul>
-                );
-            }
-        });
+         render: function() {
+             return (
+                 <nav className={"user-list user-list-"+this.state.menuVisibility}  id="user-list-nav">
+                     <MenuToggleButton onButtonClick={this.toggleMenuVisibility}  />
+                     <UserList onUserChange={this.handleUserChange} data={this.state} />
+                 </nav>
+             );
+         }
+     });
 
 
-        var User = React.createClass({
-            render: function() {
-                return (
-                    <li className={this.props.subjectUID === this.props.user.userid ? 'nav-item nav-item-active' : 'nav-item'} onClick={() => this.props.onClick(this.props.user)}>
-                        <a href="#" className="nav-link">{this.props.user.username}</a>
-                    </li>
-                );
-            }
-        });
-
-        var MenuToggleButton = React.createClass({
-            render: function() {
-                return (
-                    <button onClick={this.props.onButtonClick} className='float-right'>toggle
-                    </button>
-                );
-            }
-        });
-
-
-
+     var UserList = React.createClass({
+         render: function() {
+         //console.log("UserList: " , this);
+             let userEvent = this.props.onUserChange;
+             let subjectUID = this.props.data.subjectUID;
+             let userNodes = this.props.data.users.map(function(user) {
+                 return (
+                     <User user={user} key={user.userid} onClick={userEvent} subjectUID={subjectUID}>
+                     {user.userid}
+                     </User>
+                 );
+             });
+             return (
+                 <ul className="nav navbar-nav user-list-ul">
+                     {userNodes}
+                 </ul>
+             );
+         }
+     });
 
 
+     var User = React.createClass({
+         render: function() {
+             return (
+                 <li className={this.props.subjectUID === this.props.user.userid ? 'nav-item user-list-item user-list-item-active' : 'nav-item user-list-item'} onClick={() => this.props.onClick(this.props.user)}>
+                     <a href="#" className="nav-link">{this.props.user.username}</a>
+                 </li>
+             );
+         }
+     });
+
+     var MenuToggleButton = React.createClass({
+         render: function() {
+             return (
+                 <button onClick={this.props.onButtonClick} className='user-list-button-toggle'>
+                 <span className='user-list-icon-users' />
+                 toggle user menu
+                 </button>
+             );
+         }
+     });
+
+     ReactDOM.render(
+             <UserMenu usersUrl="api/users.json" userPollInterval={0} />,
+             document.getElementById('userListContainer')
+     );
 
 
-        ReactDOM.render(
-                <UserMenu usersUrl="api/users.json" userPollInterval={0} />,
-                document.getElementById('menuContainer')
-        );
 
-
-
-    })(React);
+ })(React);
