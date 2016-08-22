76ab0808604a05969f65cba0bfa66cae81a1d111
visegripped <nogitspam@visegripped.com>
2016-08-22 07:57:01 -0700

    Add some grunt tasks to make life easier

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,25 +9,42 @@ module.exports = function(grunt) {
                     port: 8082,
                     hostname:"127.0.0.1"
                     //livereload : 8082
-                },
-                livereload: {
-                    options: {
-                        open: true,
-                        middleware: function (connect) {
-                            return [
-                                connect.static('.tmp')
-                            ];
-                        }
-                    }
                 }
             }
+        },
+
+        less : {
+          options : {
+            sourceMap : true,
+            sourceMapFilename : 'dest/css/userlist.css.map',
+            compress : false
+          },
+          theme : {
+            expand : true,
+            flatten : true,
+            src : ["src/less/userlist.less"],
+            dest : "dist/css/",
+            ext : ".css"
+          }
+        },
+
+        watch : {
+          less : {
+            files : ['src/less/*.less'],
+            tasks : ['less'],
+            options : {
+              interrupt : false
+            }
+          }
         }
 
     });
 
     grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-    grunt.registerTask('default',['connect']);
+    grunt.registerTask('default',['connect','less','watch']);
 
 
 };
