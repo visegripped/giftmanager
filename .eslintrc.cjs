4c971cb9c2aaf89c7d0096a799b10a23ae366daf
visegripped <jt@visegripped.com>
2024-07-25 20:05:59 -0700

    Add Storybook. Scaffold a few components based

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,17 +1,7 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:@typescript-eslint/recommended-type-checked',
-    'plugin:@typescript-eslint/stylistic-type-checked',
-    'plugin:react-hooks/recommended',
-    // This disables the formatting rules in ESLint that Prettier is going to be responsible for handling.
-    // Make sure it's always the last config, so it gets the chance to override other configs.
-    'prettier',
-  ],
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:react/jsx-runtime', 'plugin:@typescript-eslint/recommended-type-checked', 'plugin:@typescript-eslint/stylistic-type-checked', 'plugin:react-hooks/recommended', 'prettier', 'plugin:storybook/recommended'],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,30 @@
+// import React from 'react';
+import './Button.css';
+
+export interface ButtonProps {
+  size?: 'small' | 'medium' | 'large';
+  label: string;
+  onClick?: () => void;
+}
+
+/**
+ * Primary UI component for user interaction
+ */
+export const Button = ({
+  size = 'medium',
+  label,
+  ...props
+}: ButtonProps) => {
+
+  return (
+    <button
+      type="button"
+      className={`button button--${size}`}
+      {...props}
+    >
+      {label}
+    </button>
+  );
+};
+
+export default Button;
