3a3a232488097dd31b75d4a6c238bacd110d1e48
visegripped <jt@visegripped.com>
2024-07-25 21:06:43 -0700

    Support for cleaner import statements

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,27 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@components': '/src/components',
+      '@pages': '/src/pages',
+      '@utilities': '/src/utilities',
+      '@context': '/src/context',
+      '@routes': '/src/routes',
+    },
+  },
   plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: ['src/__tests__/setup.ts'],
+  },
+  optimizeDeps: {
+    force: true,
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
   },
 });
