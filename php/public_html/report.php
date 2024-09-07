9f8aa2713dc2401ed79d0795f093ab7491f24782
visegripped <jt@visegripped.com>
2024-09-07 08:58:16 -0700

    report errors to remote and to user (#53)

--- php/public_html/report.php
+++ php/public_html/report.php
@@ -1,4 +1,8 @@
 <?
+header('Content-type: application/json');
+header('Access-Control-Allow-Methods: POST');
+header("Access-Control-Allow-Headers: X-Requested-With");
+header("Access-Control-Allow-Origin: '*'");
 
 include "../includes/report-credentials.php";
 
@@ -9,14 +13,13 @@ $mysqli = new mysqli("localhost", $username, $password, $database);
 
 // Check connection
 if ($mysqli->connect_errno) {
-    $apiResponse =  '{"error" : "Failed to connect to MySQL: ("' . $mysqli->connect_errno . '") "' . $mysqli->connect_error .'"}';
+     $apiResponse = array("error" => "Failed to connect to MySQL: ($mysqli->connect_errno  $mysqli->connect_error");
     exit();
 }
 
 // Prepare the SQL statement
 $query = "INSERT INTO reports (report, type, body) VALUES (?, ?, ?)";
 $stmt = $mysqli->prepare($query);
-$query = '{"warn" : "successful post with no task assignment."}';
 
 if ($stmt) {
     // Bind the parameters (s means string type)
@@ -32,26 +35,21 @@ if ($stmt) {
 
     // Check if the execution was successful
     if ($executed) {
-        $apiResponse =  '{"success":"Data successfully inserted."}';
+        $apiResponse = array("success" => "Data successfully inserted.");
     } else {
-        $apiResponse =  '{"error":"Failed to insert data: ("' . $stmt->errno .' ") "' . $stmt->error .'"}';
+        $apiResponse = array("error" => "Failed to insert data: $stmt->errno $stmt->error");
     }
 
     // Close the statement
     $stmt->close();
 } else {
-    $apiResponse =  '{"error" : "Failed to prepare the statement: ("' . $mysqli->errno . '") "' . $mysqli->error . '" }';
-}
+    $apiResponse = array("error" => "Failed to prepare the statement: ($mysqli->errno  $mysqli->error");
+    }
 
 $apiResponse = json_encode($apiResponse);
 
 // Close the connection
 $mysqli->close();
-
-
-header('Content-type: application/json');
-header('Access-Control-Allow-Methods: POST');
-header("Access-Control-Allow-Headers: X-Requested-With");
 
 
 print($apiResponse);
