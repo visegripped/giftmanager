573332aaef0780773945cc03273d69a7ff695b0b
visegripped <jt@visegripped.com>
2024-09-16 05:18:32 -0700

    remove userid from user dd. add groupId to fetch.

--- src/pages/User/User.tsx
+++ src/pages/User/User.tsx
@@ -69,6 +69,7 @@ const Table = (props: theirItemListInterface) => {
       status,
       myuserid: myUserid,
       theiruserid: theirUserid,
+      groupid: '1',
     });
     response &&
       response.then((data: responseInterface) => {
